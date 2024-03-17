dfd43c403567f78f9be20c2dbf3d5e97ff20952e
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2024-03-17 13:39:23 -0700

    Add theme and Dark Mode Toggle

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Unfonts from 'unplugin-fonts/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Unfonts({
+      google: {
+        preconnect: true,
+        display: 'block',
+        text: undefined,
+        injectTo: 'head-prepend',
+
+        // Google Fonts to Import
+        families: [
+          {
+            name: 'Montserrat',
+            styles: 'wght@200;400;500;600;700',
+            defer: true,
+          },
+        ],
+      },
+    }),
+  ],
+});
