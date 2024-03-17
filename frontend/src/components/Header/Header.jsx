9565dc21bb325eeebaa6f90b595c02f1f60a10eb
Chris Hyer <hyer.chris@gmail.com>
2024-03-17 14:08:58 -0700

    made logo clickable

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -43,50 +43,52 @@ export function HeaderSearch() {
     <header className={classes.header}>
       <Group justify={signIn ? "center" : "space-around"} className={classes.inner}>
         <Group gap={0}>
-          <Text size='30px' fw={600}>
-            ATHENS
-          </Text>
-          <Text size='30px' c='athens-blue.6' fw={400}>
-            HUB
-          </Text>
+          <Link to='/' style={{ textDecoration: "none" }}>
+            <Text size='30px' fw={600}>
+              ATHENS
+            </Text>
+            <Text size='30px' c='athens-blue.6' fw={400}>
+              HUB
+            </Text>
+          </Link>
         </Group>
         {!(signIn) && (
           <>
-        {items}
-        <TextInput
-          radius={'lg'}
-          className={classes.search}
-          placeholder='Search'
-          leftSection={
-            <IconSearch
-              style={{ width: rem(16), height: rem(16) }}
-              stroke={1.5}
+            {items}
+            <TextInput
+              radius={'lg'}
+              className={classes.search}
+              placeholder='Search'
+              leftSection={
+                <IconSearch
+                  style={{ width: rem(16), height: rem(16) }}
+                  stroke={1.5}
+                />
+              }
             />
-          }
-        />
-        <DarkMode />
+            <DarkMode />
 
-        <Button
-          key={links[links.length - 1].label}
-          // href={links[links.length - 1].link}
-          component={Link}
-          to={links[links.length - 1].link}
-          className={classes.link}
-          // onClick={(event) => event.preventDefault()}
-          variant={'filled'}
-          color={'#086a7e'}
-          radius='lg'
-          leftSection={
-            <IconLogin
-              style={{ width: rem(16), height: rem(16) }}
-              stroke={1.5}
-            />
-          }
-          label={links[links.length - 1].label}
-        >
-          {links[links.length - 1].label}
-        </Button>
-        </>
+            <Button
+              key={links[links.length - 1].label}
+              // href={links[links.length - 1].link}
+              component={Link}
+              to={links[links.length - 1].link}
+              className={classes.link}
+              // onClick={(event) => event.preventDefault()}
+              variant={'filled'}
+              color={'#086a7e'}
+              radius='lg'
+              leftSection={
+                <IconLogin
+                  style={{ width: rem(16), height: rem(16) }}
+                  stroke={1.5}
+                />
+              }
+              label={links[links.length - 1].label}
+            >
+              {links[links.length - 1].label}
+            </Button>
+          </>
         )}
       </Group>
     </header>
