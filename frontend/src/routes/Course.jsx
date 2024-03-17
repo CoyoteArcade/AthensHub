1a26649b89dd6c1ef19a512830f742fcb231032f
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2024-03-17 15:00:51 -0700

    Add Course tabs template

--- frontend/src/routes/Course.jsx
+++ frontend/src/routes/Course.jsx
@@ -1,5 +1,18 @@
-import { Breadcrumbs, Anchor, Title } from '@mantine/core';
+import {
+  Breadcrumbs,
+  Anchor,
+  Title,
+  Container,
+  Divider,
+  rem,
+  Tabs,
+} from '@mantine/core';
 import { Link, useLocation } from 'react-router-dom';
+import {
+  IconPhoto,
+  IconMessageCircle,
+  IconSettings,
+} from '@tabler/icons-react';
 
 const items = [
   { title: 'Science', link: '/subjects' },
@@ -13,12 +26,72 @@ const items = [
 function Course() {
   return (
     <>
-      <Breadcrumbs separator='→' separatorMargin='md' mt='xs'>
-        {items}
-      </Breadcrumbs>
-      <Title>Physics</Title>
+      <Container>
+        <Breadcrumbs separator='→' separatorMargin='md' mt='xs' mb='md'>
+          {items}
+        </Breadcrumbs>
+        <Title mb='lg'>Physics</Title>
+        {/* <Divider my='lg' /> */}
+
+        {/* Tabs */}
+        <CourseTabs />
+      </Container>
     </>
   );
+}
+
+function CourseTabs() {
+  const iconStyle = { width: rem(12), height: rem(12) };
+
+  return (
+    <Tabs color='gray' defaultValue='material'>
+      <Tabs.List>
+        <Tabs.Tab
+          value='material'
+          leftSection={<IconPhoto style={iconStyle} />}
+        >
+          Course Material
+        </Tabs.Tab>
+        <Tabs.Tab
+          value='psets'
+          leftSection={<IconMessageCircle style={iconStyle} />}
+        >
+          Problem Sets
+        </Tabs.Tab>
+        <Tabs.Tab
+          value='social'
+          leftSection={<IconSettings style={iconStyle} />}
+          disabled
+        >
+          Social
+        </Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value='material'>
+        <MaterialTab />
+      </Tabs.Panel>
+
+      <Tabs.Panel value='psets'>
+        <PsetTab />
+      </Tabs.Panel>
+
+      <Tabs.Panel value='social'>
+        <SocialTab />
+      </Tabs.Panel>
+    </Tabs>
+  );
+}
+
+function MaterialTab() {
+  return <>Material</>;
+}
+
+function PsetTab() {
+  return <>Psets</>;
+}
+
+function SocialTab() {
+  return <>Social</>;
 }
 
 export default Course;
