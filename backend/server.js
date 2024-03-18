f938aa61cc174eaa2593596ecded15865c830900
Yair Torres <yairtorres844@gmail.com>
2024-03-17 17:25:02 -0700

    added openai assistant

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 import express from 'express';
 import axios from 'axios';
 import mongoose from 'mongoose';
-
+import OpenAI from 'openai';
+const openai = new OpenAI();
 const app = express();
+
+app.use(express.json());
+app.listen(3001, () => {
+    console.log('Server started');
+});
+
+async function main() { 
+  const assistant = await openai.beta.assistants.create({
+    name: "Physics Professor",
+    instructions: "You are a Physics Professor who specializes in the area of Electricity and Magnetism. Please answer all questions with that in mind.",
+    tools: [{ type: "retrieval" }],
+    model: "gpt-3.5-turbo-0125"
+  });
+}
+
+
+main();
