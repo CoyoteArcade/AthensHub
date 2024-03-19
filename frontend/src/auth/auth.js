61a0610493aa191091fb68e9028931441171c5d8
Yair Torres <yairtorres844@gmail.com>
2024-03-18 19:04:38 -0700

    remade keys whoops

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -7,23 +7,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD6MDLhirm_9GmOTF_vcGjGDwKxuQySBDI",
-    authDomain: "athenshub-63330.firebaseapp.com",
-    projectId: "athenshub-63330",
-    storageBucket: "athenshub-63330.appspot.com",
-    messagingSenderId: "180591469984",
-    appId: "1:180591469984:web:d38f3d4dafecca681fb0c5",
-    measurementId: "G-MFPREZ9EDD"
-  };
-  
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_apiKey,
-//     authDomain: import.meta.env.VITE_authDomain,
-//     projectId: import.meta.env.VITE_projectId,
-//     storageBucket: import.meta.env.VITE_storageBucket,
-//     messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//     appId: import.meta.env.VITE_appId
-// };
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
